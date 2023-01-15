441a213ae077c2e1d57e198d9a2958e650ce72b8
rLukoyanov <rulukoyanov@gmail.com>
2023-01-15 14:53:03 +0300

    add profile data

--- profile.js
+++ profile.js
@@ -1,3 +1,18 @@
 const profile = {
-    
+  id: 0,
+  name: "Мария",
+  surName: "Иванова",
+
+  contacts: {
+    email: "example343@gmail.com",
+    phoneNumber: "+79080888877",
+    city: "Екатеринбург",
+    university: "Уральский федеральный университет",
+  },
+
+  advantages: ["Figma", "Illustrator", "Product design", "Workflow", "ux/ui"],
+};
+
+exports.sendProfile = (req, res) => {
+  res.send(profile);
 };
