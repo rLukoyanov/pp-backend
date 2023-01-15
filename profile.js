7e7fd69de60f31303c1c96873daf76b7f0b78478
rLukoyanov <rulukoyanov@gmail.com>
2023-01-15 18:55:34 +0300

    add work expireance

--- profile.js
+++ profile.js
@@ -13,6 +13,81 @@ const profile = {
   advantages: ["Figma", "Illustrator", "Product design", "Workflow", "ux/ui"],
 };
 
+const workExpireance = [
+  {
+    title: "React developer",
+    place: "Volonteria",
+    date: "Cентябрь 2020 - Май 2021",
+    content: `
+    Полная реализация сайта и сайта администраторов.
+    - Адаптивная верстка.
+    - Работа с WordPress
+    - Работа с RestAPI
+    - Реализация ssr на Next.js
+    - Реализация проксирования
+    
+    Стек:
+    - ReactJS
+    - Next.js
+    - Axios
+    - date-fns
+    - antd
+    - sass`,
+  },
+  {
+    title: "React developer",
+    place: "Volonteria",
+    date: "Cентябрь 2020 - Май 2021",
+    content: `
+    Полная реализация сайта и сайта администраторов.
+    - Адаптивная верстка.
+    - Работа с WordPress
+    - Работа с RestAPI
+    - Реализация ssr на Next.js
+    - Реализация проксирования
+    
+    Стек:
+    - ReactJS
+    - Next.js
+    - Axios
+    - date-fns
+    - antd
+    - sass`,
+  },
+  {
+    title: "React developer",
+    place: "Volonteria",
+    date: "Cентябрь 2020 - Май 2021",
+    content: `
+    Полная реализация сайта и сайта администраторов.
+    - Адаптивная верстка.
+    - Работа с WordPress
+    - Работа с RestAPI
+    - Реализация ssr на Next.js
+    - Реализация проксирования
+    
+    Стек:
+    - ReactJS
+    - Next.js
+    - Axios
+    - date-fns
+    - antd
+    - sass`,
+  },
+];
+
+const achievements = [
+  {
+    img: "",
+    content:
+      "Диплом призера муниципального этапа всероссийской олимпида школьников по оществознанию среди 8 классов 2019 года.",
+  },
+];
+
 exports.sendProfile = (req, res) => {
   res.send(profile);
+};
+
+exports.sendWorkExp = (req, res) => {
+  res.send(workExpireance);
 };
